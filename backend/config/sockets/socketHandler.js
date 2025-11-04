5ada6878aab5408b929c7bd739e729f1e3e4ad99
KarthikHK-01 <hkkarthik012@gmail.com>
2025-11-04 22:24:29 +0530

    Implement socket in backend

--- backend/config/sockets/socketHandler.js
+++ backend/config/sockets/socketHandler.js
@@ -1,9 +1,37 @@
 export const socketHandler = (io) => {
+    // const rooms = new Map();
+
     io.on("connection", (socket) => {
         console.log("Connected to ", socket.id);
 
-        socket.on("draw", (data) => {
-            socket.broadcast.emit("draw", data);
+        socket.on("join-room", ({roomId}) => {
+            const room = io.sockets.adapter.rooms.get(roomId);
+
+            if(!room) {
+                socket.emit("error-message", {message: `Room ID: ${roomId} not found. Creating one.....`});
+            }
+
+            socket.join(roomId);
+            console.log(`User ${socket.id} joined room `, roomId);
+
+            socket.emit("joined-room", ({roomId}));
+        });
+
+        socket.on("create-room", ({roomId}) => {
+            const room = io.sockets.adapter.rooms.get(roomId);
+
+            if(room) {
+                socket.emit("error-message", {message: `Room ID: ${roomId} already exists`});
+                return;
+            }
+            
+            socket.join(roomId);
+
+            socket.emit("created-room", ({roomId}));
+        });
+
+        socket.on("draw", ({roomId, data}) => {
+            socket.to(roomId).emit("draw", {roomId, data});
         });
 
         socket.on("disconnect", () => {
