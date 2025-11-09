9dfda176c991c76fcf3eca1fd09330de4f1b9a9d
KarthikHK-01 <hkkarthik012@gmail.com>
2025-11-09 23:55:57 +0530

    Small changes

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,13 @@ import connectDB from "./config/db.js";
 import { createServer } from "http";
 import { initSocket } from "./config/socket.js";
 import { socketHandler } from "./config/sockets/socketHandler.js";
+import dotenv from "dotenv";
 
 const app = express();
 const server = createServer(app);
 const io = initSocket(server);
+
+dotenv.config();
 
 socketHandler(io);
 
@@ -29,7 +32,7 @@ app.use(express.urlencoded({extended: true}));
 app.use("/", router);
 
 connectDB().then(() => {
-    const PORT = 9456;
+    const PORT = process.env.PORT || 9456;
     server.listen(PORT, () => {
         console.log("Server and socket connected running on 9456");
     })
