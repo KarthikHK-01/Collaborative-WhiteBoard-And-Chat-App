1f56ad3a8657dd4015766d89e40b284f92e7f33d
KarthikHK-01 <hkkarthik012@gmail.com>
2025-11-13 22:24:38 +0530

    some

--- frontend/src/config/firebase.js
+++ frontend/src/config/firebase.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// console.log("Initializing Firebase with Auth Domain:", firebaseConfig.authDomain);
+console.log("Initializing Firebase with Auth Domain:", firebaseConfig.authDomain);
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
